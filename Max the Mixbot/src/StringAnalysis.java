0f4af35daeb5a6482befe4a695419fd73f4a3f0f
archer5 <matthew.buckley@hotmail.co.uk>
2018-11-10 00:50:36 +0000

    Basic keywords definition.

--- Max the Mixbot/src/StringAnalysis.java
+++ Max the Mixbot/src/StringAnalysis.java
@@ -1,14 +1,31 @@
 public class StringAnalysis {
 
-    public static void main(String[] args) {
+    //keyword definitions
+    private String[] keywords = new String[100]; // manual for now - could be automatically updated later?
+    private void keywordDefinitions() {
+        keywords[0] = "depression";
+        keywords[1] = "drugs";
+        keywords[2] = "drink";
+        keywords[3] = "alcohol";
+        keywords[4] = "anxiety";
+        keywords[5] = "housing";
+        keywords[6] = "food";
+        keywords[7] = "sleep";
+        keywords[8] = "money";
+        keywords[9] = "parents";
+        keywords[10] = "died";
+        keywords[11] = "uni";
+        //etc.
+    }
 
+    public static void main(String[] args) {
+        StringAnalysis test = new StringAnalysis();
+        test.keywordDefinitions();
     }
 
     public String analyseString(String input) {
         //call all other methods from this
-
-        return returnString;
-
+        return input;
     }
 
 }
