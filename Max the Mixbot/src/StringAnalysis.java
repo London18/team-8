171f6856ddb38b516e1de386a6b7d2a51fb6308d
jackp1711 <34270215+jackp1711@users.noreply.github.com>
2018-11-10 00:52:51 +0000

    Search method for article API call begun.
    Connection with base URL successful.
    
    #howDidIDoThis

--- Max the Mixbot/src/StringAnalysis.java
+++ Max the Mixbot/src/StringAnalysis.java
@@ -1,14 +1,44 @@
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+
+
 public class StringAnalysis {
 
     public static void main(String[] args) {
+        StringAnalysis test = new StringAnalysis();
 
+        test.constructArticleAPICallSearchTerm("grief");
     }
 
     public String analyseString(String input) {
         //call all other methods from this
-
-        return returnString;
-
+        return input;
     }
 
+
+
+
+    public void  constructArticleAPICallSearchTerm(String searchTerm){
+
+        //Sets the https statement to be processed based on current API.
+        String coreStatement = "https://www.themix.org.uk/wp-json/wp/v2/posts?categories=28&search=";
+        String search = coreStatement + searchTerm;
+
+        //Contructs HTTP object
+        HttpURLConnection connection = null;
+
+        try{
+            URI uri = new URI(search);
+
+            URL url = new URL(uri.toURL().toString());
+
+            connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+
+        }
+        catch (Exception E){
+            System.err.println(E);
+        }
+    }
 }
